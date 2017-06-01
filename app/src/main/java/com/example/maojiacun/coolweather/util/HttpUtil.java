a6030ed0774081ee5e0386050284599a479d396a
mhyjk1 <1761497387@qq.com>
2017-06-01 12:19:31 +0800

    完成遍历省市县三级列表的功能。。

---
+++ app/src/main/java/com/example/maojiacun/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.maojiacun.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by maojiacun on 2017/5/27 0027.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request =new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
